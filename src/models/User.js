b1e710322187553e61526263e535fbb4ac13c6d4
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-18 02:36:10 +0530

    Add temporary fraud restriction support to users

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,15 +17,19 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["user", "admin", "restaurant"],
-      default: "user",
+      enum: ['user', 'admin', 'restaurant'],
+      default: 'user',
     },
     isblocked: {
       type: Boolean,
       default: false,
     },
+    fraudRestrictedUntil: {
+      type: Date,
+      default: null,
+    },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
