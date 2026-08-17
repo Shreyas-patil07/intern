3aadd721c61cb005e9be6bf5879699d2dc011026
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-18 02:45:58 +0530

    Add notification retrieval endpoints

---
+++ src/controllers/notificationController.js
@@ -0,0 +1,37 @@
+const Notification = require('../models/Notification');
+
+exports.getMyNotifications = async (req, res) => {
+  try {
+    const notifications = await Notification.find({ user: req.user._id })
+      .sort({ createdAt: -1 })
+      .limit(50);
+
+    return res.status(200).json({
+      success: true,
+      data: notifications,
+      count: notifications.length
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+exports.markAsRead = async (req, res) => {
+  try {
+    const notification = await Notification.findOneAndUpdate(
+      { _id: req.params.id, user: req.user._id },
+      { read: true },
+      { new: true }
+    );
+
+    if (!notification) {
+      return res.status(404).json({ success: false, message: 'Notification not found' });
+    }
+
+    return res.status(200).json({ success: true, data: notification });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = { getMyNotifications, markAsRead };
