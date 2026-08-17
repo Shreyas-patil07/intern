9fac10e7fe01d3087c918d48cd231f4170c17c2b
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-18 02:46:53 +0530

    Add notification API routes

---
+++ src/routes/notificationRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const { protect } = require('../middleware/authMiddleware');
+const { getMyNotifications, markAsRead } = require('../controllers/notificationController');
+
+router.use(protect);
+
+router.get('/', getMyNotifications);
+router.put('/:id/read', markAsRead);
+
+module.exports = router;
