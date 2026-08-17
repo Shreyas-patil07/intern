28553901a4485abb49dadb230dccc3df06604c1d
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-18 02:36:34 +0530

    Add order create, cancel, and refund fraud-monitoring routes

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -5,6 +5,8 @@ const { protect, authorize } = require('../middleware/authMiddleware');
 const {
   placeOrder,
   mockPayment,
+  cancelOrder,
+  requestRefund,
   getMyOrders,
   getRestaurantOrders
 } = require('../controllers/orderController');
@@ -12,8 +14,11 @@ const {
 router.use(protect);
 
 router.post('/', authorize('user'), placeOrder);
+router.post('/create', authorize('user'), placeOrder);
 router.post('/verify', authorize('user'), mockPayment);
 router.post('/:orderId/pay', authorize('user'), mockPayment);
+router.post('/cancel/:orderId', authorize('user'), cancelOrder);
+router.post('/refund/:orderId', authorize('user'), requestRefund);
 router.get('/my', authorize('user'), getMyOrders);
 router.get('/restaurant', authorize('restaurant'), getRestaurantOrders);
 
