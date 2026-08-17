33546df99857977fc5616e30624a7df9f465c71f
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-17 23:55:08 +0530

    Align order payment route with API testing workflow

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -12,6 +12,7 @@ const {
 router.use(protect);
 
 router.post('/', authorize('user'), placeOrder);
+router.post('/verify', authorize('user'), mockPayment);
 router.post('/:orderId/pay', authorize('user'), mockPayment);
 router.get('/my', authorize('user'), getMyOrders);
 router.get('/restaurant', authorize('restaurant'), getRestaurantOrders);
