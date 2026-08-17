92b73274e8b6efe87a63c99510f31bae353997d5
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-17 23:41:47 +0530

    Add order routes

---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const { protect, authorize } = require('../middleware/authMiddleware');
+const {
+  placeOrder,
+  mockPayment,
+  getMyOrders,
+  getRestaurantOrders
+} = require('../controllers/orderController');
+
+router.use(protect);
+
+router.post('/', authorize('user'), placeOrder);
+router.post('/:orderId/pay', authorize('user'), mockPayment);
+router.get('/my', authorize('user'), getMyOrders);
+router.get('/restaurant', authorize('restaurant'), getRestaurantOrders);
+
+module.exports = router;
