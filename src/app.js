5a3d5fdc59654b05783882b29baa8db3fbeff864
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-17 23:21:20 +0530

    Wire restaurant routes into Express app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const restaurantRoutes = require('./routes/restaurantRoute');
 
 const app = express();
 
@@ -9,9 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
+app.use('/api/restaurants', restaurantRoutes);
 
 app.get('/', (req, res) => {
-  res.status(200).json({ success: true, message: 'Welcome to Swiggy API' });
+  res.status(200).json({
+    success: true,
+    message: 'Welcome to Swiggy API'
+  });
 });
 
 module.exports = app;
