1971802951660fe670076c768a8438ee7e3fc413
Shreyasrp07 <3shreyas2007@gmail.com>
2026-08-18 02:41:57 +0530

    Add delivery partner user role

--- src/models/User.js
+++ src/models/User.js
@@ -2,32 +2,16 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      select: false,
-    },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true, select: false },
     role: {
       type: String,
-      enum: ['user', 'admin', 'restaurant'],
-      default: 'user',
+      enum: ['user', 'admin', 'restaurant', 'delivery'],
+      default: 'user'
     },
-    isblocked: {
-      type: Boolean,
-      default: false,
-    },
-    fraudRestrictedUntil: {
-      type: Date,
-      default: null,
-    },
+    isblocked: { type: Boolean, default: false },
+    fraudRestrictedUntil: { type: Date, default: null }
   },
   { timestamps: true }
 );
